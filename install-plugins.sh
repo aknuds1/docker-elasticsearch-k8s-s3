859492545f653ec60965233530dace75809667f6
Arve Knudsen <arve.knudsen@gmail.com>
2017-07-09 15:52:12 +0200

    Try to fix plugin install

--- install-plugins.sh
+++ install-plugins.sh
@@ -2,4 +2,6 @@
 export MINIMUM_MASTER_NODES=2
 export HTTP_PORT=9200
 export TRANSPORT_PORT=9300
+export NODE_DATA=true
+export NODE_MASTER=true
 gosu elasticsearch /elasticsearch/bin/elasticsearch-plugin install repository-s3
