c4a585c6d9b039b5a815f9d46902f13c8eca71e4
Arve Knudsen <arve.knudsen@gmail.com>
2017-07-10 10:30:05 +0200

    Debugging

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo $USERNAME
+ls -ld config/
+ls -l config/
 sysctl -w vm.max_map_count=262144
 ./bin/elasticsearch_logging_discovery >> ./config/elasticsearch.yml
 su - elasticsearch
