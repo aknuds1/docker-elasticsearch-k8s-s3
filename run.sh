fde7520b0ffeae79cd4f5305c82ebb75fa8ca287
Arve Knudsen <arve.knudsen@gmail.com>
2017-07-10 15:10:09 +0200

    Run ES as user elasticsearch

--- run.sh
+++ run.sh
@@ -6,15 +6,6 @@ export HTTP_PORT=${HTTP_PORT:-9200}
 export TRANSPORT_PORT=${TRANSPORT_PORT:-9300}
 export MINIMUM_MASTER_NODES=${MINIMUM_MASTER_NODES:-2}
 
-ls -ld config/
-ls -l config/
 sysctl -w vm.max_map_count=262144
 ./bin/elasticsearch_logging_discovery >> ./config/elasticsearch.yml
-echo elasticsearch.yml:
-cat config/elasticsearch.yml
-echo '\njvm.options':
-cat config/jvm.options
-echo '\nlog42.properties':
-cat config/log4j2.properties
-su - elasticsearch
-exec ./bin/es-docker
+exec su - elasticsearch -c ./bin/es-docker
