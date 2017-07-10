7c9c016acb08579aa523a23b7ef43c510911ddbf
Arve Knudsen <arve.knudsen@gmail.com>
2017-07-10 14:59:09 +0200

    Add missing config

--- run.sh
+++ run.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-whoami
+export NODE_NAME=${NODE_NAME:-${HOSTNAME}}
+export NODE_MASTER=${NODE_MASTER:-true}
+export NODE_DATA=${NODE_DATA:-true}
+export HTTP_PORT=${HTTP_PORT:-9200}
+export TRANSPORT_PORT=${TRANSPORT_PORT:-9300}
+export MINIMUM_MASTER_NODES=${MINIMUM_MASTER_NODES:-2}
+
 ls -ld config/
 ls -l config/
-cat config/elasticsearch.yml
-cat config/jvm.options
-cat config/log4j2.properties
 sysctl -w vm.max_map_count=262144
 ./bin/elasticsearch_logging_discovery >> ./config/elasticsearch.yml
+echo elasticsearch.yml:
+cat config/elasticsearch.yml
+echo '\njvm.options':
+cat config/jvm.options
+echo '\nlog42.properties':
+cat config/log4j2.properties
 su - elasticsearch
 exec ./bin/es-docker
