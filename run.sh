87b4a0f15b0d81a87622dd7d136093e737b21874
Arve Knudsen <arve.knudsen@gmail.com>
2017-08-01 14:36:20 +0200

    Upgrade to 5.5.1

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh
+set -ex
+
 export NODE_NAME=${NODE_NAME:-${HOSTNAME}}
 export NODE_MASTER=${NODE_MASTER:-true}
 export NODE_DATA=${NODE_DATA:-true}
@@ -9,4 +11,4 @@ export MINIMUM_MASTER_NODES=${MINIMUM_MASTER_NODES:-2}
 chown -R elasticsearch:elasticsearch /data
 
 ./bin/elasticsearch_logging_discovery >> ./config/elasticsearch.yml
-exec gosu elasticsearch ./bin/es-docker
+exec su elasticsearch -c ./bin/es-docker
